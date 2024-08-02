6178257ec7a289ad6d42d59770c0a8902b2c3582
WLemmy <wilfred.lemayian@gmail.com>
2024-08-02 14:58:30 +0300

    add products category

--- backend-urban-bazaar/models.py
+++ backend-urban-bazaar/models.py
@@ -25,3 +25,17 @@ class Brand (db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     products = db.relationship('Product', back_populates='brand')
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(255))
+    image_url = db.Column(db.String(255))
+    price = db.Column(db.Integer, nullable=False)
+    category_name = db.Column(db.String(120), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('Category', back_populates='products')
+    tags = db.relationship('Tags')
+    orders = db.relationship('Order', back_populates='product', cascade='all, delete-orphan')
+    users = association_proxy('orders', 'user', creator=lambda user_obj: Order(user=user_obj))
