f2e891887f772b979e69364d7f4223212c37cf88
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-02 16:32:47 +0300

    Add a function to convert price strings to float values

--- backend-urban-bazaar/seed.py
+++ backend-urban-bazaar/seed.py
@@ -6,3 +6,7 @@ from faker import Faker
 from datetime import datetime
 from app import db, create_app, bcrypt
 from models import User, Product, Order, Category, Checkout, Contact, Role, Brand
+
+def convert_price_to_float(price_str):
+    numeric_part = re.sub(r'[^\d.,]', '', price_str).replace(',', '')
+    return float(numeric_part)
