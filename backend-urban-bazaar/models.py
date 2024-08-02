840b6e8549605d7118e5b40c2a0c21b28f14bff2
WLemmy <wilfred.lemayian@gmail.com>
2024-08-02 15:00:53 +0300

    add order model

--- backend-urban-bazaar/models.py
+++ backend-urban-bazaar/models.py
@@ -39,3 +39,16 @@ class Product(db.Model):
     tags = db.relationship('Tags')
     orders = db.relationship('Order', back_populates='product', cascade='all, delete-orphan')
     users = association_proxy('orders', 'user', creator=lambda user_obj: Order(user=user_obj))
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Integer, nullable=False)
+    order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    status = db.Column(db.String(20), nullable=False, default="unprocessed")  # New column
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    user = db.relationship('User', back_populates='orders')
+    product = db.relationship('Product', back_populates='orders')
