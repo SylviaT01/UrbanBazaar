354d1113413d618f03242a5ac10410bec075541c
WLemmy <wilfred.lemayian@gmail.com>
2024-08-02 15:10:57 +0300

    edit contact model

--- backend-urban-bazaar/models.py
+++ backend-urban-bazaar/models.py
@@ -69,7 +69,6 @@ class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    rating = db.Column(db.Integer, nullable=False, default=0)
     message = db.Column(db.String(500), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship('User', back_populates='contacts')
