a6579afb927c1d745c8f6da3d2498ecff50eb2f8
Sylvia Chebet <sylviachebet03@gmail.com>
2024-08-02 18:49:27 +0300

    Update `NavItems` component to display links for "Join UrbanBazaar," "About us," and "Get in Touch" on the upper right side of the navbar.Adjust flex container to use `justify-end` for right alignment of links.

--- frontend-urban-bazaar/src/components/navItems.jsx
+++ frontend-urban-bazaar/src/components/navItems.jsx
@@ -4,8 +4,19 @@ import { Link } from 'react-router-dom';
 export default function NavItems() {
   return (
     <nav className="bg-blue-100 border-b border-gray-300">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-8">
+      <div className="container mx-auto px-4 sm:px-6 lg:px-1">
+        <div className="flex h-8 items-center justify-end">
+          <div className="flex space-x-4">
+            <Link to="/join" className="text-gray-700 hover:text-blue-700">
+              Join UrbanBazaar
+            </Link>
+            <Link to="/about" className="text-gray-700 hover:text-blue-700">
+              About us
+            </Link>
+            <Link to="/contact" className="text-gray-700 hover:text-blue-700">
+              Get in Touch
+            </Link>
+          </div>
         </div>
       </div>
     </nav>
