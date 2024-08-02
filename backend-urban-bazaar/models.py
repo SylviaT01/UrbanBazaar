f1ff7947a81bf29393474f2387fcc87dc5fec800
WLemmy <wilfred.lemayian@gmail.com>
2024-08-02 14:42:21 +0300

    add user models

--- backend-urban-bazaar/models.py
+++ backend-urban-bazaar/models.py
@@ -1,3 +1,16 @@
-import flask from Flask
+from app import db
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    orders = db.relationship('Order', back_populates='user', cascade='all, delete-orphan')
+    addresses = db.relationship('Address', back_populates='user')
+    products = association_proxy('orders', 'product', creator=lambda product_obj: Order(product=product_obj))
+    contacts = relationship('Contact', back_populates='user')
 
