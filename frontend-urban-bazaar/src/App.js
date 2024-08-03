12b5a7fb7646ace404819698d314df833233e60d
mrrsmburu <mrrsmburu@gmail.com>
2024-08-03 09:18:28 +0300

    created login form

--- frontend-urban-bazaar/src/App.js
+++ frontend-urban-bazaar/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+// import Home from "./Pages/home";
+// import AboutUs from "./Pages/about";
+// import Contact from "./Pages/contact";
+// import WishList from "./components/wishList";
+// import ProductList from "./components/productList";
+// import CartModal from "./components/cartModal";
+import Login from "./Forms /login"
+
+
+
+
+function AppContent() {
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      < Login />
+      <div className="flex-grow">
+        <Routes>
+          <Route path="/login" element={<Login />} /> 
+          
+          {/* <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} /> */}
+          {/* <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/cart" element={<CartModal />} />
+          <Route path="/wishlist" element={<WishList />} />
+          <Route path="/products" element={<ProductList />} /> */}
+        </Routes>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+          <AppContent />
+    </Router>
   );
 }
 
