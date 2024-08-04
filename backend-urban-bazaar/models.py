9a1b50b1a25a14cf58a55a8e1ec4fa324df4d8e5
Uwes Ahabab <uwesahabab@gmail.com>
2024-08-04 18:37:30 +0300

    ShippingDetails model to store shipping information for orders

--- backend-urban-bazaar/models.py
+++ backend-urban-bazaar/models.py
@@ -106,16 +106,15 @@ class OrderItem(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float, nullable=False)
 
-class checkout(db.Model):
-    __tablename__ = 'addresses'
+# ShippingDetails model to store shipping information for orders
+class ShippingDetails(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), nullable=False)
-    phone_number = db.Column(db.Integer, nullable=False)
-    city = db.Column(db.String(120), nullable=False)
-    street_address = db.Column(db.String(120), nullable=False)
-    apartment= db.Column(db.String(120), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    user = db.relationship('User', back_populates='addresses') 
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    street_address = db.Column(db.String(255), nullable=False)
+    apartment_number = db.Column(db.String(100), nullable=True)
+    city = db.Column(db.String(100), nullable=False)
+    zip_code = db.Column(db.String(20), nullable=False)
 
 class Contact(db.Model):
     __tablename__ = 'contacts'
