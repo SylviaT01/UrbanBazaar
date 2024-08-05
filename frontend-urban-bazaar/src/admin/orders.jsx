8e9d5e9184972dd859f9f77481f025bf26230538
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-06 00:04:51 +0300

    Import the icons

---
+++ frontend-urban-bazaar/src/admin/orders.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from "react";
+import logo from "../assets/logo.svg";
+import search from "../assets/search.svg";
+import darkmode from "../assets/darkmode.svg";
+import bell from "../assets/bell.svg";
+import profile1 from "../assets/profile1.svg";
+
