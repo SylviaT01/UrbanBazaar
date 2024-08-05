6662c630cc7f8b73efdfd11fc7ac702e0b21890c
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-06 00:05:53 +0300

    Add a button for the dark mode functionality

--- frontend-urban-bazaar/src/admin/orders.jsx
+++ frontend-urban-bazaar/src/admin/orders.jsx
@@ -5,3 +5,21 @@ import darkmode from "../assets/darkmode.svg";
 import bell from "../assets/bell.svg";
 import profile1 from "../assets/profile1.svg";
 
+const DarkModeToggle = ({ isDarkMode, toggleDarkMode }) => {
+    return (
+      <button
+        type="button"
+        onClick={toggleDarkMode}
+        className="flex items-center justify-center bg-transparent border-none p-0"
+      >
+        <img
+          src={darkmode}
+          className={`object-contain w-6 aspect-square ${
+            isDarkMode ? "filter invert" : ""
+          }`}
+          alt="Darkmode"
+        />
+      </button>
+    );
+  };
+
