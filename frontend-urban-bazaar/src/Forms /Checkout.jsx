1cb502facfcfb571273883940536588d62d0d499
mrrsmburu <mrrsmburu@gmail.com>
2024-08-05 22:19:18 +0300

    adding form

--- frontend-urban-bazaar/src/Forms /Checkout.jsx
+++ frontend-urban-bazaar/src/Forms /Checkout.jsx
@@ -15,3 +15,20 @@ function Checkout() {
             <div className="bg-black text-white rounded-full w-6 h-6 flex items-center justify-center">1</div>
             <span className="ml-2 text-gray-600">cart</span>
           </div>
+          <div className="flex items-center">
+            <div className="bg-gray-300 rounded-full w-6 h-6 flex items-center justify-center">2</div>
+            <span className="ml-2 text-gray-600">checkout</span>
+          </div>
+        </div>
+
+        <div className="flex mb-6">
+          <div className="w-1/2 mr-4">
+            <h2 className="text-lg font-semibold mb-4">Shipping Address</h2>
+            <form>
+              <div className="flex mb-4">
+                <label className="mr-2 flex items-center">
+                  <input type="radio" name="address" className="mr-2" />
+                  Add New Address
+                </label>
+              </div>
+
