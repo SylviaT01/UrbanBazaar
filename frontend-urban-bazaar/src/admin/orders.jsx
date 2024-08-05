e5296d1e37811a7ec03a517388a17f5796295461
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-06 00:26:07 +0300

    Add the search input

--- frontend-urban-bazaar/src/admin/orders.jsx
+++ frontend-urban-bazaar/src/admin/orders.jsx
@@ -52,6 +52,27 @@ const Orders = () => {
           className="object-contain shrink-0 mt-1.5 aspect-[1.83] w-[75px]"
           alt="Logo"
         />
+        <div className="flex flex-col items-start h-[41px] gap-50">
+          <div className="flex px-20 flex-wrap gap-50 justify-between items-center w-full max-w-[301px]">
+            <div className="flex items-center bg-stone-300 rounded h-[42px] min-w-[240px] w-[301px] px-2">
+              <button
+                type="button"
+                className="flex items-center justify-center bg-transparent border-none p-0"
+              >
+                <img
+                  src={search}
+                  className="object-contain w-6 aspect-square mr-2"
+                  alt="Search"
+                />
+              </button>
+              <input
+                type="text"
+                placeholder="Search"
+                className="flex-grow bg-transparent border-none outline-none"
+              />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
