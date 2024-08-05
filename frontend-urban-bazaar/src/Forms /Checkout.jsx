ce208bf24c5db985014c99721ae3257a62087844
mrrsmburu <mrrsmburu@gmail.com>
2024-08-05 22:14:46 +0300

    add return and divs

--- frontend-urban-bazaar/src/Forms /Checkout.jsx
+++ frontend-urban-bazaar/src/Forms /Checkout.jsx
@@ -6,3 +6,12 @@ function Checkout() {
   const handlePaymentMethodChange = (method) => {
     setPaymentMethod(method);
   };
+
+  return (
+    <div className="bg-gray-100 p-8 min-h-screen">
+      <div className="container mx-auto bg-white p-6 rounded-md shadow-md">
+        <div className="flex justify-between mb-4">
+          <div className="flex items-center">
+            <div className="bg-black text-white rounded-full w-6 h-6 flex items-center justify-center">1</div>
+            <span className="ml-2 text-gray-600">cart</span>
+          </div>
