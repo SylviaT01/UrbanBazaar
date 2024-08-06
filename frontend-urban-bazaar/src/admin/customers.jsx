b80860c1d8d91d6d380a99115bde74ba4bb38d7a
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-06 14:52:39 +0300

    Fetch user data from the Flask API

--- frontend-urban-bazaar/src/admin/customers.jsx
+++ frontend-urban-bazaar/src/admin/customers.jsx
@@ -3,7 +3,14 @@ import profile2 from "../assets/profile2.svg";
 import members from "../assets/members.svg";
 
 const Customer = () => {
-    const [users, setUsers] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const usersPerPage = 10;
-}    
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 10;
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/user")
+      .then((response) => response.json())
+      .then((data) => setUsers(data))
+      .catch((error) => console.error("Error fetching users:", error));
+  }, []);
+};
