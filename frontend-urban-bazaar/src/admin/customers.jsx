06ec2b67e17c65a4d17be958a6388bee444be122
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-06 15:04:01 +0300

    Add div's for the main content area & render

--- frontend-urban-bazaar/src/admin/customers.jsx
+++ frontend-urban-bazaar/src/admin/customers.jsx
@@ -21,4 +21,19 @@ const Customer = () => {
   const totalPages = Math.ceil(users.length / usersPerPage);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div className="flex overflow-hidden flex-col items-center px-20 bg-sky-50 max-md:px-5">
+      <div className="mt-10 w-full max-w-[1240px] max-md:mt-10 max-md:max-w-full">
+        <div className="flex gap-5 max-md:flex-col">
+          <div className="flex flex-col w-[22%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col grow pb-8 mt-5 font-medium whitespace-nowrap max-md:mt-10"></div>
+          </div>
+          <div className="flex flex-col ml-5 w-[78%] max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col w-full max-md:mt-10 max-md:max-w-full"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
