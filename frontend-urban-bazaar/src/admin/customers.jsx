7dad5d7f77235be00729d5105069a21b5decd0b6
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-06 14:51:08 +0300

    State to store user data, track the current page for pagination & number of users to display per page

--- frontend-urban-bazaar/src/admin/customers.jsx
+++ frontend-urban-bazaar/src/admin/customers.jsx
@@ -1,3 +1,9 @@
 import React, { useState, useEffect } from "react";
 import profile2 from "../assets/profile2.svg";
 import members from "../assets/members.svg";
+
+const Customer = () => {
+    const [users, setUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const usersPerPage = 10;
+}    
