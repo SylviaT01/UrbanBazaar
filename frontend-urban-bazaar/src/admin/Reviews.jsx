254e512e81f7494ba2eee712ed5081dcdea51169
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-07 16:14:49 +0300

    Arrow function & initialize state variables

--- frontend-urban-bazaar/src/admin/Reviews.jsx
+++ frontend-urban-bazaar/src/admin/Reviews.jsx
@@ -2,4 +2,9 @@ import React, { useState, useEffect } from "react";
 import profile from "../assets/profile.svg";
 import Rating from "react-rating-stars-component";
 
-
+const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [reviewsPerPage] = useState(6);
+  const [selectedReviews, setSelectedReviews] = useState([]);
+};
