ccd4a791310a7ac0b7e53d1fd78ad222415a52aa
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-07 16:15:38 +0300

    Fetch reviews from Flask API

--- frontend-urban-bazaar/src/admin/Reviews.jsx
+++ frontend-urban-bazaar/src/admin/Reviews.jsx
@@ -8,3 +8,10 @@ const Reviews = () => {
   const [reviewsPerPage] = useState(6);
   const [selectedReviews, setSelectedReviews] = useState([]);
 };
+
+useEffect(() => {
+  fetch("http://127.0.0.1:5000/review")
+    .then((response) => response.json())
+    .then((data) => setReviews(data.reviews))
+    .catch((error) => console.error("Error fetching reviews:", error));
+}, []);
