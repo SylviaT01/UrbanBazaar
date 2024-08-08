5c9497802050e3ce0f950f1ea2d6f5b16b57d630
WLemmy <wilfred.lemayian@gmail.com>
2024-08-08 13:03:48 +0300

    Add print button to allow users to print the current view of payment data

--- frontend-urban-bazaar/src/admin/Payments.jsx
+++ frontend-urban-bazaar/src/admin/Payments.jsx
@@ -36,7 +36,37 @@ useEffect(() => {
       setCurrentPage(currentPage - 1);
     }
   };
-  
+  const handlePrint = () => {
+  window.print();
+    <div>
+      {/* Table content */}
+      <div>
+        <button 
+          onClick={handlePrint} 
+          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+        >
+          Print
+        </button>
+        {/* Pagination buttons */}
+      </div>
+    </div>
+  );
+};
+
+return (
+  <div>
+    {/* Table content */}
+    <div>
+      <button 
+        onClick={handlePrint} 
+        className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+      >
+        Print
+      </button>
+      {/* Pagination buttons */}
+    </div>
+  </div>
+);
   const handleNext = () => {
     if (currentPage < Math.ceil(payments.length / paymentsPerPage)) {
       setCurrentPage(currentPage + 1);
