49c98d786cc183f576879acc5285a4e0d36550b3
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-09 11:42:22 +0300

    Render the title and print button

--- frontend-urban-bazaar/src/Product/orderComplete.jsx
+++ frontend-urban-bazaar/src/Product/orderComplete.jsx
@@ -24,25 +24,44 @@ const OrderComplete = () => {
 
   const products = [
     {
-      imageSrc: shoe ,
+      imageSrc: shoe,
       name: "Shoes",
       description: "Ultraboost Light Running Shoes",
       quantity: 2,
       price: "Ksh 1,300",
     },
     {
-      imageSrc: shoe ,
+      imageSrc: shoe,
       name: "Shoes",
       description: "Ultraboost Light Running Shoes",
       quantity: 2,
       price: "Ksh 1,300",
     },
     {
-      imageSrc: shoe ,
+      imageSrc: shoe,
       name: "Shoes",
       description: "Ultraboost Light Running Shoes",
       quantity: 2,
       price: "Ksh 1,300",
     },
   ];
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-sky-50 px-4">
+      <div className="flex flex-col bg-white p-8 rounded-lg shadow-lg max-w-6xl w-full">
+        <header className="...">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            Your order is complete!
+          </h1>
+          <button
+            className="..."
+            onClick={handlePrint} // Triggering print functionality
+          >
+            <img src={print} alt="Print" className="w-8 h-8" />
+            <span>Print</span>
+          </button>
+        </header>
+      </div>
+    </div>
+  );
 };
