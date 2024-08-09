aaa8adc123a14f174a820c450e7eb7785c99007e
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-09 11:37:54 +0300

    Create a reference to the main content to be printed & set up the print functionality

--- frontend-urban-bazaar/src/Product/orderComplete.jsx
+++ frontend-urban-bazaar/src/Product/orderComplete.jsx
@@ -2,3 +2,23 @@ import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
 import print from "../assets/print.svg";
 import shoe from "../assets/shoe.svg";
+
+const OrderComplete = () => {
+  const printRef = useRef();
+
+  const handlePrint = useReactToPrint({
+    content: () => printRef.current,
+    documentTitle: "Order Summary",
+    pageStyle: `
+        @page {
+          size: A4;
+          margin: 20mm;
+        }
+        @media print {
+          body {
+            -webkit-print-color-adjust: exact;
+          }
+        }
+      `,
+  });
+};
