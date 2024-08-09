5bf43db41929549407c1d74ebe2e22ae55ee605a
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-09 11:46:13 +0300

    Add the shipping info section & specify the content to be printed

--- frontend-urban-bazaar/src/Product/orderComplete.jsx
+++ frontend-urban-bazaar/src/Product/orderComplete.jsx
@@ -61,6 +61,70 @@ const OrderComplete = () => {
             <span>Print</span>
           </button>
         </header>
+
+        {/* Main content to be printed */}
+        <main ref={printRef}>
+          <section className="self-center px-8 py-0.5 mt-7 bg-white rounded-lg border border-solid border-gray-200 w-full max-w-[1240px] max-md:px-5">
+            <div className="flex flex-wrap md:flex-nowrap gap-1">
+              <section className="flex flex-col w-full md:w-1/2">
+                <h2 className="text-3xl font-semibold mb-5 mt-3">
+                  Shipping Info
+                </h2>
+                <hr className="flex border-t border-gray-300 mb-5 w-80" />
+                <div className="text-xl text-gray-700 space-y-5">
+                  <p>
+                    Name:{" "}
+                    <span className="font-medium text-gray-900">John Doe</span>
+                  </p>
+                  <p>
+                    Phone:{" "}
+                    <span className="font-medium text-gray-900">
+                      +254123456789
+                    </span>
+                  </p>
+                  <p>
+                    City:{" "}
+                    <span className="font-medium text-gray-900">
+                      Nairobi, Kenya
+                    </span>
+                  </p>
+                  <p>
+                    Company Name:{" "}
+                    <span className="font-medium text-gray-900">
+                      Urbanbazaar Ltd
+                    </span>
+                  </p>
+                </div>
+              </section>
+              <section className="flex flex-col w-full md:w-1/2">
+                <div className="text-xl text-gray-700 space-y-5 py-20">
+                  <p>
+                    Email:{" "}
+                    <span className="font-medium text-blue-600">
+                      johndoe@company.com
+                    </span>
+                  </p>
+                  <p>
+                    Zip:{" "}
+                    <span className="font-medium text-gray-900">01110</span>
+                  </p>
+                  <p>
+                    Address:{" "}
+                    <span className="font-medium text-gray-900">
+                      Cross road towers, mweni road
+                    </span>
+                  </p>
+                  <p>
+                    Company Phone:{" "}
+                    <span className="font-medium text-gray-900">
+                      +25412345678
+                    </span>
+                  </p>
+                </div>
+              </section>
+            </div>
+          </section>
+        </main>
       </div>
     </div>
   );
