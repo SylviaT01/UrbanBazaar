2b4bfda0d713801a0bf189cb1767f92636a93520
WLemmy <wilfred.lemayian@gmail.com>
2024-08-09 13:16:13 +0300

    Register Chart.js components to enable the necessary features for the SalesChart component.

--- frontend-urban-bazaar/src/admin/SalesChart.jsx
+++ frontend-urban-bazaar/src/admin/SalesChart.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from "chart.js";
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
 
 function SalesChart({ orders }) {
   // Prepare data for the chart
