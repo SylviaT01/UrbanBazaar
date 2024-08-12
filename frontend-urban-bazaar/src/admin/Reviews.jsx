81ff13bdd145d41a6811ec8c565cdcd962b29357
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-12 14:16:11 +0300

    Remove the delete option

--- frontend-urban-bazaar/src/admin/Reviews.jsx
+++ frontend-urban-bazaar/src/admin/Reviews.jsx
@@ -22,32 +22,6 @@ const Reviews = () => {
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-  const handleSelectReview = (reviewId) => {
-    setSelectedReviews((prevSelectedReviews) =>
-      prevSelectedReviews.includes(reviewId)
-        ? prevSelectedReviews.filter((id) => id !== reviewId)
-        : [...prevSelectedReviews, reviewId]
-    );
-  };
-
-  const handleDeleteSelected = () => {
-    fetch("http://127.0.0.1:5000/delete_reviews", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ review_ids: selectedReviews }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setReviews((prevReviews) =>
-          prevReviews.filter((review) => !selectedReviews.includes(review.id))
-        );
-        setSelectedReviews([]);
-      })
-      .catch((error) => console.error("Error deleting reviews:", error));
-  };
-
   const pageNumbersToShow = 5; // Number of page buttons to show
   const halfPageNumbersToShow = Math.floor(pageNumbersToShow / 2);
 
@@ -77,15 +51,6 @@ const Reviews = () => {
         Reviews
       </div>
       <div className="-mt-12 w-full max-w-[1239px] max-md:max-w-full">
-        <div className="flex justify-end mb-4">
-          <button
-            className="px-4 py-2 bg-red-500 text-white rounded"
-            onClick={handleDeleteSelected}
-            disabled={selectedReviews.length === 0}
-          >
-            Delete Selected
-          </button>
-        </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 max-md:flex-col">
           {currentReviews.map((review) => (
             <div
@@ -97,11 +62,6 @@ const Reviews = () => {
               key={review.id}
             >
               <div className="flex items-center gap-3">
-                <input
-                  type="checkbox"
-                  checked={selectedReviews.includes(review.id)}
-                  onChange={() => handleSelectReview(review.id)}
-                />
                 <img
                   src={Profile}
                   alt="Reviewer"
