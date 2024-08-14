8905bb289cfe058320de56073d369b062c5d0b41
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-08-15 02:04:26 +0300

    Implement the is_admin logic

--- frontend-urban-bazaar/src/Forms /login.jsx
+++ frontend-urban-bazaar/src/Forms /login.jsx
@@ -1,30 +1,40 @@
-import React, { useState, useContext } from 'react';
-import { UserContext } from '../contexts/userContext';
+import React, { useState, useContext } from "react";
+import { UserContext } from "../contexts/userContext";
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 import supermarket from "../assets/supermarket.jpg.jpg";
-import googleIcon from '../assets/google icon.png';
-import facebookIcon from '../assets/facebookIcon.png';
-
+import googleIcon from "../assets/google icon.png";
+import facebookIcon from "../assets/facebookIcon.png";
 
 export default function Login() {
   const { login } = useContext(UserContext);
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
-  function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    login(email, password);
-  }
+    try {
+      const user = await login(email, password);
+      if (user) {
+        if (user.is_admin === true) {
+          navigate("/dashboard");
+        } else {
+          navigate("/"); 
+        }
+      }
+    } catch (error) {
+      console.error("Login failed:", error);
+      
+    }
+  };
 
   const handleGoHome = () => {
     navigate("/");
   };
-
 
   return (
     <div className="login-container">
@@ -36,10 +46,15 @@ export default function Login() {
               <div className="flex items-center justify-center md:h-full">
                 <div className="w-full max-w-md">
                   <div className="bg-white overflow-hidden">
-                    <h2 className="text-3xl font-semibold mb-4 text-center">Log in</h2>
+                    <h2 className="text-3xl font-semibold mb-4 text-center">
+                      Log in
+                    </h2>
                     <form className="space-y-4" onSubmit={handleSubmit}>
                       <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                        <label
+                          htmlFor="email"
+                          className="block text-sm font-medium text-gray-700"
+                        >
                           Email address
                         </label>
                         <input
@@ -55,13 +70,16 @@ export default function Login() {
                         />
                       </div>
                       <div className="relative">
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+                        <label
+                          htmlFor="password"
+                          className="block text-sm font-medium text-gray-700"
+                        >
                           Password
                         </label>
                         <input
                           id="password"
                           name="password"
-                          type={showPassword ? 'text' : 'password'}
+                          type={showPassword ? "text" : "password"}
                           autoComplete="current-password"
                           required
                           value={password}
@@ -69,8 +87,13 @@ export default function Login() {
                           className="input-field border border-gray-300 rounded-md px-3 py-2 w-full"
                           placeholder="Password"
                         />
-                        <span className="absolute inset-y-0 right-0 pr-3 pt-4 flex items-center cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
-                          <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                        <span
+                          className="absolute inset-y-0 right-0 pr-3 pt-4 flex items-center cursor-pointer"
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          <FontAwesomeIcon
+                            icon={showPassword ? faEyeSlash : faEye}
+                          />
                         </span>
                       </div>
                       <div>
@@ -89,32 +112,50 @@ export default function Login() {
                             type="checkbox"
                             className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
                           />
-                          <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                          <label
+                            htmlFor="remember-me"
+                            className="ml-2 block text-sm text-gray-900"
+                          >
                             Remember me
                           </label>
                         </div>
                         <div className="text-sm">
-                          <Link to="#" className="font-medium text-blue-500 hover:text-blue-400">
+                          <Link
+                            to="#"
+                            className="font-medium text-blue-500 hover:text-blue-400"
+                          >
                             Forgot your password?
                           </Link>
                         </div>
                       </div>
                       <div>
-
                         <div className="flex items-center justify-center mt-4 space-x-4 ">
                           <button className="flex items-center justify-center py-1 px-3 w-auto text-black rounded-md shadow-sm text-sm border border-gray-200">
-                            <img src={googleIcon} alt="Google" className="w-5 h-5 mr-2" />
+                            <img
+                              src={googleIcon}
+                              alt="Google"
+                              className="w-5 h-5 mr-2"
+                            />
                             Google
                           </button>
-                          <button className="flex items-center justify-center py-1 px-3 w-auto text-black  rounded-md shadow-sm text-sm border border-gray-200">
-                            <img src={facebookIcon} alt="Facebook" className="w-4 h-4 mr-2" />
+                          <button className="flex items-center justify-center py-1 px-3 w-auto text-black rounded-md shadow-sm text-sm border border-gray-200">
+                            <img
+                              src={facebookIcon}
+                              alt="Facebook"
+                              className="w-4 h-4 mr-2"
+                            />
                             Facebook
                           </button>
                         </div>
                       </div>
                       <div className="text-center mt-4">
-                        <span className="text-sm text-gray-600">Don't have an account?</span>{' '}
-                        <Link to="/signup" className="text-sm font-medium text-blue-500 hover:text-blue-400">
+                        <span className="text-sm text-gray-600">
+                          Don't have an account?
+                        </span>{" "}
+                        <Link
+                          to="/signup"
+                          className="text-sm font-medium text-blue-500 hover:text-blue-400"
+                        >
                           Sign up
                         </Link>
                       </div>
@@ -132,12 +173,24 @@ export default function Login() {
               </div>
             </div>
             {/* Left blue half */}
-
-            <div className="relative flex flex-col justify-center items-center md:w-1/2 px-4 py-4 md:px-8 bg-cover bg-center opacity-70" style={{ height: '500px', backgroundImage: `url(${supermarket})`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat' }}>
+            <div
+              className="relative flex flex-col justify-center items-center md:w-1/2 px-4 py-4 md:px-8 bg-cover bg-center opacity-70"
+              style={{
+                height: "500px",
+                backgroundImage: `url(${supermarket})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+              }}
+            >
               <div className="absolute inset-0 opacity-50"></div>
               <div className="relative z-10">
-                <h2 className="text-3xl font-semibold text-black text-center">Welcome to UrbanBazaar</h2>
-                <p className="text-black mt-4 text-center">Your one stop shop for all you need.</p>
+                <h2 className="text-3xl font-semibold text-black text-center">
+                  Welcome to UrbanBazaar
+                </h2>
+                <p className="text-black mt-4 text-center">
+                  Your one stop shop for all you need.
+                </p>
               </div>
             </div>
           </div>
